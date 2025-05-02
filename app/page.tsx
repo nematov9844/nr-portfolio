1bc131ebd5b54e1b68512c602cda7af22028b3d0
Kaito <jeki2004114r@gmail.com>
2025-05-02 17:55:11 +0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <div className="bg-gradient-to-br to-red-500 from-blue-600 h-screen w-full flex justify-center items-center">Home</div>
+  )
+}
+
+export default Home
