ef922409732766f3996d913e0e3d43877cae8411
Kaito <jeki2004114r@gmail.com>
2025-05-02 18:11:23 +0500

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+'use client'
+
+import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
-    <div className="bg-gradient-to-br to-red-500 from-blue-600 h-screen w-full flex justify-center items-center">Home</div>
+    <main className="h-screen w-full bg-gradient-to-br from-[#0f0c29] via-[#302b63] to-[#24243e] flex items-center justify-center">
+      <motion.h1
+        className="text-white text-center text-3xl sm:text-5xl font-bold"
+        animate={{ y: [0, -15, 0] }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          repeatType: "loop",
+        }}
+      >
+        NR style'ni <br className="sm:hidden" /> portfolio sahifasi
+      </motion.h1>
+    </main>
   )
 }
 
